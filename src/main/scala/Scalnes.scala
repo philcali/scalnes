3fd813242ea97904b095c95f41487bbbd20700e4
Philip Cali <pcali1@lsu.edu>
2010-08-27 15:04:21 -0500

    Upgraded to 2.8 and added some Documentation.

--- src/main/scala/Scalnes.scala
+++ src/main/scala/Scalnes.scala
@@ -5,7 +5,14 @@ import java.awt.image.BufferedImage
 import java.awt.Color
 import javax.imageio.ImageIO
 
+/**
+ * NES object to load a NES rom
+ <pre>
+val lickle = NES load "lickle.nes"
+ </pre>
+ */
 object NES {
+  /** Provide a full path to a NES rom binary */
   def load(file: String) = {
     val fis = new FileInputStream(file)
     fis.skip(4L)
@@ -17,9 +24,16 @@ object NES {
     fis.skip(9L)
 
     if(trainer != 0) fis.skip(512L)
-    fis.skip(pgr * 16384)
 
-    val chrbanks = new Array[Byte](8192 * chr)
+    // If the chrbanks is 0, then the sprites are
+    // embedded in the pgr blocks
+    val multiplier = if(chr == 0) pgr else { 
+      // skip the pgr garbage
+      fis.skip(pgr * 16384) 
+      chr
+    }
+
+    val chrbanks = new Array[Byte](8192 * multiplier)
     val read = fis.read(chrbanks)
     fis.close
 
@@ -27,16 +41,23 @@ object NES {
   }
 }
 
+/** An NES class to parse out NES tiles 
+<pre>
+lickle process >> "allsprites.gif"
+val banks_3 = lickle process 3
+val banks_0_10 = lickle process(0 to 10)
+</pre>
+*/
 class NES(val chr: Int, val data: Array[Byte]) {
   def process(id: Int) = {
     val location = id * 8192
-    new BankSegment(data.subArray(location, (location + 1) + 8192), false)
+    new BankSegment(data slice(location, (location + 1) + 8192), false)
   }
 
   def process(r: Range) = {
-    val start = r.first * 8192
+    val start = r.head * 8192
     val size = (start + r.size) * 8192
-    new BankSegment(data.subArray(start, size), false)
+    new BankSegment(data slice(start, size), false)
   }
 
   def process = {
@@ -46,6 +67,11 @@ class NES(val chr: Int, val data: Array[Byte]) {
   def >> (filename: String) = process >> (filename)
 }
 
+/** A Character Bank Segment that contains sprite info. This class actually saves the sprites to a file
+<pre>
+banks_3 tiles(375 to 435) horizontally >> "lickle_fonts.gif"
+</pre>
+*/
 class BankSegment(val data: Array[Byte], val horizontal: Boolean) {
   val tileNo = data.length / 16
   val (x, y) = {
@@ -57,7 +83,7 @@ class BankSegment(val data: Array[Byte], val horizontal: Boolean) {
   def horizontally = new BankSegment(data, true)
 
   def tiles(ts: Range) = {
-    new BankSegment(data subArray(ts.first * 16, (ts.last + 1) * 16), horizontal)
+    new BankSegment(data slice(ts.head * 16, (ts.last + 1) * 16), horizontal)
   }
 
   def >> (filename: String) = process(filename)
